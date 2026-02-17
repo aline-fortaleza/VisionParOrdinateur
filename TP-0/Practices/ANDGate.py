6ab17bb30b25dd3106916a33a80c79bd8466e640
aline-fortaleza <affs2@cin.ufpe.br>
2026-02-17 12:46:22 +0100

    first steps

--- TP-0/Practices/ANDGate.py
+++ TP-0/Practices/ANDGate.py
@@ -7,12 +7,10 @@
 def activationFunction(n):
 
     #TODO - Application 1 - Step 4b - Define the binary step function as activation function
-
-
-
-
-
-    return
+    if n >= 0:
+        return 1
+    else:
+        return 0
 #####################################################################################################################
 #####################################################################################################################
 
@@ -24,10 +22,11 @@ def forwardPropagation(p, weights, bias):
     a = None # the neuron output
 
     # TODO - Application 1 - Step 4a - Multiply weights with the input vector (p) and add the bias   =>  n
-
+    n = weights[0]*p[0] + weights[1]*p[1] + bias 
 
 
     # TODO - Application 1 - Step 4c - Pass the result to the activation function  =>  a
+    a = activationFunction(n)
 
 
 
@@ -56,36 +55,41 @@ def main():
     t = [0, 0, 0, 1]
 
     #TODO - Application 1 - Step 2 - Initialize the weights with zero  (weights)
-
+    w = [0,0] # weights for the two inputs
+    
 
     #TODO - Application 1 - Step 2 - Initialize the bias with zero  (bias)
-
+    b = 0 # bias
 
     #TODO - Application 1 - Step 3 - Set the number of training steps  (epochs)
-    epochs = 100
+    epochs = 50  
 
     #TODO - Application 1 - Step 4 - Perform the neuron training for multiple epochs
     for ep in range(epochs):
         for i in range(len(t)):
 
             #TODO - Application 1 - Step 4 - Call the forwardPropagation method
-
+            a = forwardPropagation(P[i], w, b)
 
             #TODO - Application 1 - Step 5 - Compute the prediction error (error)
-
+            error = t[i] - a
 
             #TODO - Application 1 - Step 6 - Update the weights
-
+            w[0] = w[0] + error * P[i][0]
+            w[1] = w[1] + error * P[i][1]
 
             #TODO - Application 1 - Step 7 - Update the bias
-
-            # DELETE THIS
-            continue
+            b = b + error # bnew = bold + error 
+            
 
     #TODO - Application 1 - Step 8 - Print weights and bias
-
+    print("Trained weights: ", w)
+    print("Trained bias: ", b)
    
     # TODO - Application 1 - Step 9 - Display the results
+    for i in range(len(t)):
+        a = forwardPropagation(P[i], w, b)
+        print("Input: ", P[i], " Predicted values: ", a, " Ground truth label: ", t[i])
 
    
     return
